b15bf901df5f9c2af811b5bfd545972d58a8fa73
Alison Williams <alison.lauriewill@gmail.com>
2019-07-23 11:45:27 +0100

    Initial Review Container file created and rendering in App.js

---
+++ src/container/ReviewsContainer.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+
+class ReviewsContainer extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {}
+
+  }
+
+
+
+  render() {
+    return(
+      <h1>Hello World!</h1>
+    )
+  }
+}
+
+
+export default ReviewsContainer
