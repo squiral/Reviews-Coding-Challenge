38d2fe5a76cd5942e6bb390ba88e947842795e47
Alison Williams <alison.lauriewill@gmail.com>
2019-07-29 11:30:49 +0100

    attempting unit testing. imported enzyme package and react-test-renderer

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import ReviewsContainer from './container/ReviewsContainer.js'
+import { shallow } from 'enzyme';
+import './setupTests.js'
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('App renders without crashing', () => {
+  shallow(<App />);
 });
+
+
+it('can sort by word frequency', () => {
+  const container = shallow(<ReviewsContainer />)
+  const array = [{text: "yes", value: 10}, {text: "no", value: 12}]
+  expect(sortByFrequency(array)).toEqual([{text: "no", value: 12}, {text: "yes", value:10}])
+})
